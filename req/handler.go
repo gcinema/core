1220c9d4eab2b336b1a1ce11451cb1db8cfa9ba2
Zakhar Guskov <guskov.zahar20@gmail.com>
2026-03-13 17:17:29 +0500

    feat: add req handler

---
+++ req/handler.go
@@ -0,0 +1,18 @@
+package req
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type StructValidator interface {
+	Struct(s any) error
+}
+
+func DecodeBody(req *http.Request, body *any) error {
+	return json.NewDecoder(req.Body).Decode(body)
+}
+
+func Validate(body any, validator StructValidator) error {
+	return validator.Struct(body)
+}
--- res/handler.go
+++ res/handler.go
@@ -1,4 +1,4 @@
-package httphelper
+package res
 
 import (
 	"encoding/json"
